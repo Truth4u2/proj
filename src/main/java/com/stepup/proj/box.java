2f687bcc7fe56a6510e1f4617e10c045b290a996
Nikita Kvasov <grobovskoi@mail.ru>
2025-08-11 17:36:08 +0300

    Изменена основная логика Добавлен класс коробки

---
+++ src/main/java/com/stepup/proj/box.java
@@ -0,0 +1,20 @@
+package com.stepup.proj;
+
+public class box {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public box() {
+    }
+
+    public box(int size) {
+        this.size = size;
+    }
+}
